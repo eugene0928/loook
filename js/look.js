ecf7fadf743af5cdcfdbce035b1790429947a650
eugene0928 <umid09tuxtayev@gmail.com>
2022-01-31 19:01:52 +0500

    all functions are implemented

--- js/look.js
+++ js/look.js
@@ -1,33 +1,145 @@
-const navbar = document.querySelector('.navbar-main')
-const customers_list = document.querySelector('.customers-list')
-const user_add = document.querySelector('#userAdd')
-const userInput = document.querySelector('#usernameInput')
-const phone = document.querySelector('#telephoneInput')
-const main_div = document.querySelector('.main')
 const btn = document.querySelector('#orders')
-const main_orders = document.querySelector('.main-orders')
+const main_div = document.querySelector('.main')
 const ul = document.querySelector('.orders-list')
+const user_add = document.querySelector('#userAdd')
+const foodForm = document.querySelector('#foodsForm')
 const select = document.querySelector('#foodsSelect')
+const navbar = document.querySelector('.navbar-main')
 const foodsCount = document.querySelector('#foodsCount')
+const main_orders = document.querySelector('.main-orders')
+const userInput = document.querySelector('#usernameInput')
+const customers_list = document.querySelector('.customers-list')
+const telephoneInput = document.querySelector('#telephoneInput')
 
-const clientId_span = document.querySelector('#clientId')
+const clientId = document.querySelector('#clientId')
 const username_h1 = document.querySelector('#userHeader')
 
-const foods = ["Chicken tog'ora", "Chicken wings", "Burger cheese", "Combo", "Spinner", "Cola", "Fanta"]
+const foods = ["chicken_togora", "chicken_wings", "burger_cheese", "combo", "spinner", "cola", "fanta"]
+let users = window.localStorage.getItem('users')
+users  = JSON.parse(users) || [{userId: 1, username: 'Umid', contact: '998936215272', order:{cola: 4}}, {userId: 2, username: 'eugene', contact: '998936215272', order:{fanta:5}}]
+
+
+function renderUsers(users) {
+    customers_list.innerHTML = null
+    for(let user of users) {
+        const [li, span, a] = createElement('li', 'span', 'a')
+
+        span.textContent = user.username;
+        a.textContent = '+' + user.contact
+
+        li.classList.add('customer-item')
+        span.classList.add('customer-name')
+        a.classList.add('customer-phone')
+        a.setAttribute('href', 'tel:+' + user.contact)
+
+        li.append(span, a);
+        customers_list.append(li)
+
+        li.onclick = () => {
+            clientId.textContent = user.userId;
+            username_h1.textContent = user.username;
+            const filteredOrder = user.order
+
+            renderOrders(filteredOrder)
+        }
+    }
+}
+
+/* client ordersni main divga chiqaruvchi function  */
+function renderOrders(orders) {
+    ul.innerHTML = null
+
+    for(let order in orders) {
+        const [li, img, div, count, name] = createElement('li', 'img', 'div', 'span', 'span')
+        
+        count.textContent = orders[order];
+        name.textContent = order
+        
+        li.classList.add('order-item')
+        name.classList.add('order-name')
+        count.classList.add('order-count')
+
+        img.src = './img/' + order + '.jpeg';
+        div.append(name, count)
+        li.append(img, div)
+        ul.append(li)
+    }
+}
+
+function renderFoods(arr) {
+    for(let order of arr) {
+        const [option] = createElement('option')
+        option.textContent = order;
+        option.value = order;
+
+        select.append(option)
+    }
+}
+
+user_add.onsubmit = () => {
+    event.preventDefault()
+    if(valid_name(userInput.value) && valid_num(telephoneInput.value)) {
+        users.push({
+            userId: users.length ? +(users.at(-1).userId) + 1 : 1,
+            username: userInput.value,
+            contact: telephoneInput.value,
+            order: {}
+        })
+
+        window.localStorage.setItem('users', JSON.stringify(users))
+
+        userInput.value = null;
+        telephoneInput.value = null;
+
+        renderUsers(users)
+    } else {
+        alert('Invalid username or phone number')
+    }
+}
+
+foodForm.onsubmit = () => {
+    event.preventDefault()
+    if(clientId.textContent) {
+        let user = users.find( el => el.userId == clientId.textContent)
+
+        if(user['order'][select.value]) {
+            user['order'][select.value] = +(user['order'][select.value]) + +foodsCount.value;
+        } else {
+            user['order'][select.value] = foodsCount.value;
+        }
+        window.localStorage.setItem('users', JSON.stringify(users))
+
+        foodsCount.value = 1
+        renderOrders(user.order)
+    }
+}
+
+
+function createElement(...arr) {
+    return arr.map( el => document.createElement(el))
+}
+
+
+
 
 
 function valid_name(value) {
-
     if(value.trim() && value.length <= 30 && value.length >= 3 && (/^[a-z A-Z]+$/gi.test(value))) return true;
     return false;
 }
 
 function valid_num(value) {
 
-    if(/^[0-9]+$/gi.test(value) && value.length === 9) return true;
+    if((/^998[389][0123456789][0-9]{7}$/).test(value)) return true;
     return false;
 }
 
 String.prototype.toProperCase = function () {
     return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 };
+
+
+
+renderUsers(users)
+renderFoods(foods)
+// renderOrders()
